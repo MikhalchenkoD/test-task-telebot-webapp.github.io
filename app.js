9ae5cb9a742b15df265353b34a013a0b602515b5
Михальченко Дмитрий <140504256+MikhalchenkoD@users.noreply.github.com>
2023-11-29 13:49:50 +0600

    Update app.js

---
+++ app.js
@@ -0,0 +1,38 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = '#FFFFFF';
+tg.MainButton.color = '#2cab37';
+
+let item = "";
+
+let btnMario = document.getElementById("mario");
+let btnAlbert = document.getElementById("albert");
+
+btnMario.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали Марио!");
+		item = "mario";
+		tg.MainButton.show();
+	}
+});
+
+btnAlbert.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали Альберта Энштейна!");
+		item = "albert";
+		tg.MainButton.show();
+	}
+});
+
+
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+	tg.sendData(item);
+});
